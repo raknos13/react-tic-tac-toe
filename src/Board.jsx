6e5ebb21e918b174effc6725c83653176117d0ef
Mukesh Kumar <mkso13189@gmail.com>
2024-07-10 16:08:38 +0530

    added coordinates on move history

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,6 @@
 import Square from "./Square";
 
-export default function Board({ xIsNext, squares, onPlay, currentMove }) {
+export default function Board({ xIsNext, squares, onPlay, currentMove, calculateWinner }) {
 	const winnerInfo = calculateWinner(squares);
 	const winner = winnerInfo ? winnerInfo.winner : null;
 	const winningLine = winnerInfo ? winnerInfo.line : [];
@@ -15,7 +15,7 @@ export default function Board({ xIsNext, squares, onPlay, currentMove }) {
 		} else {
 			nextSquares[i] = "O";
 		}
-		onPlay(nextSquares);
+		onPlay(nextSquares, i);
 	}
 
 	let status;
@@ -28,6 +28,7 @@ export default function Board({ xIsNext, squares, onPlay, currentMove }) {
 	}
 	console.log(winner, winningLine);
 
+	// dynamically generate 3x3 grid using two loops.
 	const boardRows = [...Array(3).keys()].map((row) => {
 		const boardSquares = [...Array(3).keys()].map((col) => {
 			const i = 3 * row + col;
@@ -55,25 +56,4 @@ export default function Board({ xIsNext, squares, onPlay, currentMove }) {
 			{boardRows}
 		</>
 	);
-}
-
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return { winner: squares[a], line: lines[i] };
-		}
-	}
-
-	return null;
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -3,13 +3,13 @@ import Board from "./Board";
 
 export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true);
-	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [history, setHistory] = useState([{squares: Array(9).fill(null), index: -1}]);
 	const [currentMove, setCurrentMove] = useState(0);
 	const [reverseMoveOrder, setReverseMoveOrder] = useState(false);
-	const currentSquares = history[currentMove];
+	const currentSquares = history[currentMove].squares;
 
-	function handlePlay(nextSquares) {
-		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+	function handlePlay(nextSquares, i) {
+		const nextHistory = [...history.slice(0, currentMove + 1), {squares: nextSquares, index: i}];
 		setHistory(nextHistory);
 		setCurrentMove(nextHistory.length - 1);
 		setXIsNext(!xIsNext);
@@ -20,12 +20,17 @@ export default function Game() {
 		setXIsNext(nextMove % 2 == 0);
 	}
 
-	const moves = history.map((squares, move) => {
+	const moves = history.map((squaresInfo, move) => {
 		let description;
+		const index = squaresInfo.index;
+		const row = Math.floor(index / 3);
+		const col = index % 3;
+		const symbol = index % 2 == 0 ? 'X' : 'O';
+		console.log('index: ', index);
 		if (move > 0) {
-			description = "Goto move #" + move;
+			description = `Goto move #${move} ${symbol}(${row}, ${col})`;
 		} else {
-			description = "Goto game start";
+			description = "Goto game start (0, 0)";
 		}
 		return (
 			<li key={move}>
@@ -46,6 +51,7 @@ export default function Game() {
 					squares={currentSquares}
 					onPlay={handlePlay}
 					currentMove={currentMove}
+					calculateWinner={calculateWinner}
 				/>
 			</div>
 			<div className='game-info'>
@@ -56,4 +62,25 @@ export default function Game() {
 			</div>
 		</div>
 	);
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return { winner: squares[a], line: lines[i] };
+		}
+	}
+
+	return null;
 }
